5385395fe9c97cf133420c11ffc8991b6c2c1e56
sinem.avci <sinem.avci@ekinokssoftware.com>
2026-01-26 18:55:01 +0300

    feat: aar implement

--- android/src/main/java/com/paintboxreactnative/PaintBoxReactNativeModule.kt
+++ android/src/main/java/com/paintboxreactnative/PaintBoxReactNativeModule.kt
@@ -1,5 +1,6 @@
 package com.paintboxreactnative
 
+import com.kotlin.native_drawing_plugin.PaintBoxView
 import com.facebook.react.bridge.ReactApplicationContext
 
 class PaintBoxReactNativeModule(reactContext: ReactApplicationContext) :
